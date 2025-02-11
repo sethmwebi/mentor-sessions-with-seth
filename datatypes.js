2ee0a439a26388a75e3834a838a33cf3d0f375c4
Seth Mwebi <sethmwebi27@gmail.com>
2025-02-11 13:03:18 +0300

    javascript

---
+++ datatypes.js
@@ -0,0 +1,24 @@
+// string
+// let president = "Trump";
+let vicePresident = "Vance";
+let number = "4";
+// number
+let num = 22;
+let pi = 3.142;
+// boolean => true, false
+let isLoggedIn = false;
+// undefined
+let message;
+// console.log(message);
+// null
+let user = null;
+// symbol
+let hello = Symbol("hello");
+// console.log(hello);
+// object
+/* const president = {
+  name: "Donald",
+  age: 79,
+};
+
+console.log(typeof isLoggedIn); */
---
+++ index.js
@@ -0,0 +1,41 @@
+// let name = "Donald";
+// var age = 20;
+// const teacher = "joseph";
+//
+// let president;
+// let num;
+//
+// num = 22;
+// let number = 20;
+//
+// console.log(num);
+// console.log(number);
+// const student1 = "john";
+// const student2 = "mark";
+// const student3 = "joseph";
+// const student1 = "john",
+//   student2 = "mark",
+//   student3 = "joseph";
+//
+// console.log(student1, student2, student3);
+// var name = "seth";
+// console.log(name);
+// name = "kevin";
+// console.log(name);
+// const message = "hello";
+// const _message2 = "world";
+// const $message3 = "president";
+//
+// const message1 = "hi"
+// const const = 5;
+// for, const, let, var
+// const num = 20;
+
+// num = 10;
+// function calculateProduct(num1, num2) {
+//   return num1 * num2;
+// }
+//
+// console.log(calculateProduct(5, 6));
+let teacher = "Titus";
+console.log("hey now brown cow");
